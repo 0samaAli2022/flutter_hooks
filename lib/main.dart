bf35c4094f114141e0191e419cbe1f26d0255a15
osama <osamaali31121999@gmail.com>
2023-04-10 23:14:21 +0200

    step-2 countdown

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:flutter_hooks_test/count_down.dart';
 
 extension CompactMap<T> on Iterable<T?> {
   Iterable<T> compactMap<E>([
@@ -22,34 +22,18 @@ void main() {
   );
 }
 
-const url = 'https://bit.ly/3qYOtDm';
-
 class HomePage extends HookWidget {
   const HomePage({super.key});
   @override
   Widget build(BuildContext context) {
-    final future = useMemoized(
-      () => NetworkAssetBundle(Uri.parse(url))
-          .load(url)
-          .then(
-            (data) => data.buffer.asUint8List(),
-          )
-          .then(
-            (data) => Image.memory(data),
-          ),
-    );
-    final snapshot = useFuture(future);
-
+    final countDown = useMemoized(() => CountDown(from: 20));
+    final notifier = useListenable(countDown);
     return Scaffold(
       appBar: AppBar(
         title: const Text('Home Page'),
         centerTitle: true,
       ),
-      body: Column(
-        children: [
-          snapshot.data,
-        ].compactMap().toList(),
-      ),
+      body: Text(notifier.value.toString()),
     );
   }
 }
